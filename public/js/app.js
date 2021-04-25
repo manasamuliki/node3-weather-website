2516067b2b40807da3ae4e2ab88d4c62994c7366
Manasa <manasamuliki@gmail.com>
2021-04-25 21:43:24 +0530

    commit msg

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,8 @@
 
 fetch('http://puzzle.mead.io/puzzle').then( (response) => {
     response.json().then((data) => {
-        console.log(data)
+        console.log(data);
+        console.log('hhhh');
     })
 })
 
