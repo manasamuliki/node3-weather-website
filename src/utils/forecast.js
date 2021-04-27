0ed502d55abe7c49518343b0482abc46aeac2d00
Manasa <manasamuliki@gmail.com>
2021-04-27 15:29:27 +0530

    Add humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,9 @@ const forecast = (latitude,longitude,callback) => {
         } else if (body.error) {
             callback('Unable to find the location try another one!', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out. It feels like " +  body.current.feelslike + " degress out.")
+            callback(undefined, 
+                body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out. It feels like " + 
+                body.current.feelslike + " degress out. The humidity is " + body.current.humidity + "%. ")
             
         }
         })
